d7e4c45d34466c3c4cdbb341269656097cacde47
Fenomen-Alex <alexfenomen@gmail.com>
2022-01-10 19:58:05 +0200

    Updates

---
+++ precache-manifest.aad0b1a818ed1fb94bbd802d10c7e320.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0a39613c2d28479cf5d7aa2f160a87f",
+    "url": "/GamePRS/index.html"
+  },
+  {
+    "revision": "47375b140ab3eaba4488",
+    "url": "/GamePRS/static/css/main.c17a1eaf.chunk.css"
+  },
+  {
+    "revision": "38b63845285aa863597e",
+    "url": "/GamePRS/static/js/2.b95c2202.chunk.js"
+  },
+  {
+    "revision": "3405545c67c2b04913629256d4fb5d95",
+    "url": "/GamePRS/static/js/2.b95c2202.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47375b140ab3eaba4488",
+    "url": "/GamePRS/static/js/main.a15f76a8.chunk.js"
+  },
+  {
+    "revision": "52e25d506db4f0e4bd53",
+    "url": "/GamePRS/static/js/runtime-main.35b41550.js"
+  }
+]);
